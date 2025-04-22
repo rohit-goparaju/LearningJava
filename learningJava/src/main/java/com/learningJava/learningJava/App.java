e68f9d9b54ca9fc1e468e9d69dc3a086cbb1b005
Rohit <rohitgoparaju.r96@gmail.com>
2025-04-22 17:01:28 +0530

    add ConsoleRollingFileAppender and ConsoleLogs

--- learningJava/src/main/java/com/learningJava/learningJava/App.java
+++ learningJava/src/main/java/com/learningJava/learningJava/App.java
@@ -11,8 +11,18 @@ import org.apache.logging.log4j.Logger;
 public class App {
 
 	public static final Logger log = LogManager.getLogger(App.class);
-	
+
 	public static void main(String[] args) {
-		log.info("Hello World!");
-    }
+
+		for(int i = 0; i < 10 ; i++)
+		{
+			log.info("Hello World!");
+			try {
+				Thread.sleep(1000);
+			}catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
 }
