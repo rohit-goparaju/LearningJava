73100e8058044f7c509bbca77cf38c0012705285
Rohit <rohitgoparaju.r96@gmail.com>
2025-04-22 14:35:49 +0530

    add log4j2

--- learningJava/src/main/java/com/learningJava/learningJava/App.java
+++ learningJava/src/main/java/com/learningJava/learningJava/App.java
@@ -1,10 +1,18 @@
 package com.learningJava.learningJava;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  */
+
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+	public static final Logger log = LogManager.getLogger(App.class);
+	
+	public static void main(String[] args) {
+		log.info("Hello World!");
     }
 }
