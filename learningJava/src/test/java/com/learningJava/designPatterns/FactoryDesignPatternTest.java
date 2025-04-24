8c49578dca970d691995fa50a4e199dccabaf46d
Rohit <rohitgoparaju.r96@gmail.com>
2025-04-24 12:26:44 +0530

    add FactoryDesignPatternTest class

---
+++ learningJava/src/test/java/com/learningJava/designPatterns/FactoryDesignPatternTest.java
@@ -0,0 +1,23 @@
+package com.learningJava.designPatterns;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class FactoryDesignPatternTest {
+	FactoryDesignPattern shapeFactory = new FactoryDesignPattern();
+	
+	@Test
+	public void circleTest() {
+		
+		Shape myCircle= shapeFactory.getShape("Circle",10);
+		assertTrue(myCircle.getDisplayString().equals("Circle with radius 10"));
+	}
+	
+	@Test
+	public void triangleTest() {
+		Shape myTriangle = shapeFactory.getShape("Triangle",10);
+		assertTrue(myTriangle.getDisplayString().equals("Triangle with side 10"));
+	}
+	
+}
