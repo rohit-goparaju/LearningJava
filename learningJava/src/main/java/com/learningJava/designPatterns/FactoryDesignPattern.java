b00a43c3273dac2593f5d2b7eac938e0c8d01e25
Rohit <rohitgoparaju.r96@gmail.com>
2025-04-24 11:38:12 +0530

    Update FactoryDesignPattern with Shape class

--- learningJava/src/main/java/com/learningJava/designPatterns/FactoryDesignPattern.java
+++ learningJava/src/main/java/com/learningJava/designPatterns/FactoryDesignPattern.java
@@ -1,61 +1,48 @@
 package com.learningJava.designPatterns;
 
-import java.util.Optional;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class FactoryDesignPattern {
-
-	public static enum PlanType{
-		DOMESTIC, INSTITUTIONAL, COMMERCIAL
-	};
-
-	
-	public Optional<Plan> getPlan(PlanType planType) {
-		switch(planType) {
-		case PlanType.DOMESTIC:
-			return Optional.of(new DomesticPlan());
-		case PlanType.INSTITUTIONAL:
-			return Optional.of(new InstitutionalPlan());
-		case PlanType.COMMERCIAL: 
-			return Optional.of(new CommercialPlan());
-		default: 
-			return Optional.empty();
+	public Shape getShape(String shapeType) {
+		switch(shapeType) {
+		case "Circle": return new Circle();
+		case "Triangle": return new Triangle();
+		default: return null;
 		}
 	}
 }
 
-abstract class Plan{
-	public static final Logger log = LoggerFactory.getLogger(Plan.class);
-	protected double rate;
-	
-	abstract void getRate();
-	
-	public void calculateBill(int units) {
-		double bill = rate * units;
-		log.info("Bill for " + units + " units is : " + String.valueOf(bill));
+abstract class Shape{
+	public static final Logger log = LoggerFactory.getLogger(Shape.class);
+	String name;
+
+	abstract void setName();
+
+	void display() {
+		log.info(name);
 	}
 }
 
-class DomesticPlan extends Plan{
+class Circle extends Shape{
+
+	Circle(){
+		this.setName();
+	}
+
 	@Override
-	public void getRate() {
-		rate = 3.5;
+	void setName() {
+		this.name = "Circle";
 	}
 }
 
-class InstitutionalPlan extends Plan{
-	@Override
-	void getRate() {
-		rate = 5.5;
+class Triangle extends Shape{
+	Triangle(){
+		this.setName();
 	}
-}
 
-class CommercialPlan extends Plan{
 	@Override
-	void getRate() {
-		rate = 7.5;
+	void setName() {
+		this.name = "Triangle";
 	}
 }
