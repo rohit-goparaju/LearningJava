075f6cbc33937c5e95de5194d20a0af8c2522c03
Rohit <rohitgoparaju.r96@gmail.com>
2025-05-05 12:05:46 +0530

    update DecoratorDesignPattern with a phone example.

--- learningJava/src/main/java/com/learningJava/designPatterns/DecoratorDesignPattern.java
+++ learningJava/src/main/java/com/learningJava/designPatterns/DecoratorDesignPattern.java
@@ -1,53 +1,54 @@
 package com.learningJava.designPatterns;
 
-public class DecoratorDesignPattern extends CoffeeDecorator {
-	public DecoratorDesignPattern(Coffee coffee) {
-		super(coffee);
+public class DecoratorDesignPattern extends PhoneDecorator {
+	
+	public DecoratorDesignPattern(Phone phone) {
+		super(phone);
 	}
 	
 	@Override
-	public String getDesctription() {
-		return super.getDesctription() + " milk";
+	public String getFeatures() {
+		return super.getFeatures() + ", 50MP ultrawide, 100x telescopic zoom, AI features, Ultra tag";
 	}
 	
 	@Override
-	public double getCost() {
-		return super.getCost() + 5;
+	public Double getCost() {
+		return super.getCost() + 40000;
 	}
+	
 }
 
-interface Coffee{
-	String getDesctription();
-	double getCost();
+interface Phone{
+	String getFeatures();
+	Double getCost();
 }
 
-class SimpleCoffee implements Coffee{
+class SamsungPhone implements Phone{
 	@Override
-	public double getCost() {
-		return 10;
+	public String getFeatures() {
+		return "5000mah battery, 200MP rear camera, ip 68 water resistance";
 	}
 	
 	@Override
-	public String getDesctription() {
-		return "Simple coffee";
-	}
-}
-
-abstract class CoffeeDecorator implements Coffee{
-	Coffee decoratedCoffee;
-	public CoffeeDecorator(Coffee coffee) {
-		this.decoratedCoffee = coffee;
-	}
-	
-	@Override
-	public double getCost() {
-		return decoratedCoffee.getCost();
-	}
-	
-	@Override
-	public String getDesctription() {
-		return decoratedCoffee.getDesctription();
+	public Double getCost() {
+		return 60000.00;
 	}
 }
 
 
+abstract class PhoneDecorator implements Phone{
+	Phone decoratedPhone;
+	public PhoneDecorator(Phone phone) {
+		this.decoratedPhone = phone;
+	}
+	
+	@Override
+	public String getFeatures() {
+		return decoratedPhone.getFeatures();
+	}
+	
+	@Override
+	public Double getCost() {
+		return decoratedPhone.getCost();
+	}
+}
--- learningJava/src/test/java/com/learningJava/designPatterns/DecoratorDesignPatternTest.java
+++ learningJava/src/test/java/com/learningJava/designPatterns/DecoratorDesignPatternTest.java
@@ -9,13 +9,15 @@ public class DecoratorDesignPatternTest {
 
 	@Test
 	void test() {
-		SimpleCoffee simpleCoffee = new SimpleCoffee();
-		DecoratorDesignPattern milkCoffee = new DecoratorDesignPattern(simpleCoffee);
+		SamsungPhone phone = new SamsungPhone();
 		
-		assertTrue(simpleCoffee.getDesctription().equals("Simple coffee"));
-		assertTrue(milkCoffee.getDesctription().equals("Simple coffee milk"));
-		assertEquals(10, simpleCoffee.getCost());
-		assertEquals(15, milkCoffee.getCost());
+		DecoratorDesignPattern phoneUltra = new DecoratorDesignPattern(new SamsungPhone());
+		
+		assertTrue(phone.getFeatures().equals("5000mah battery, 200MP rear camera, ip 68 water resistance"));
+		assertEquals(60000, phone.getCost());
+		
+		assertTrue(phoneUltra.getFeatures().equals("5000mah battery, 200MP rear camera, ip 68 water resistance"+", 50MP ultrawide, 100x telescopic zoom, AI features, Ultra tag"));
+		assertEquals(100000, phoneUltra.getCost());
 	}
 	
 }
