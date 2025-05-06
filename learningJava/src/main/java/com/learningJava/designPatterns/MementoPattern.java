6315107dcc025d51315caca47aa68fdcf22445fe
Rohit <rohitgoparaju.r96@gmail.com>
2025-05-06 18:18:06 +0530

    add MementoPattern

---
+++ learningJava/src/main/java/com/learningJava/designPatterns/MementoPattern.java
@@ -0,0 +1,59 @@
+package com.learningJava.designPatterns;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MementoPattern {
+
+}
+
+class Originator{
+	String state;
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	public Memento saveStateToMemento() {
+		return new Memento(state);
+	}
+	
+	public void getStateFromMemento(Memento memento) {
+		this.state = memento.getState();
+	}
+}
+
+class Memento{
+	String state;
+	
+	public Memento(String state) {
+		super();
+		this.state = state;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+}
+
+
+class Caretaker{
+	
+	List<Memento> mementos = new ArrayList<>();
+	
+	boolean addMemento(Memento memento) {
+		return mementos.add(memento);
+	}
+	
+	Memento getMemento(int index) {
+		return mementos.get(index);
+	}
+}
