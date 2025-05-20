c91224694ecbe9396d4c16158a528778c6a45066
Rohit <rohitgoparaju.r96@gmail.com>
2025-05-20 20:10:18 +0530

    add getRandomString method in R96Random class.

--- learningJava/src/main/java/com/r96/util/R96Random.java
+++ learningJava/src/main/java/com/r96/util/R96Random.java
@@ -7,4 +7,15 @@ public class R96Random {
 	public static int[] getRandomIntArr(int size) {
 		return Random.from(RandomGenerator.getDefault()).ints(size, 0, size).toArray();
 	}
+
+	public static String getRandomString(int length) {
+		StringBuilder sb = new StringBuilder(length);
+
+		Random rand = new Random();
+
+		for(int i = 0; i < length; i++) 
+			sb.append((char)rand.nextInt(97, 122));
+
+		return sb.toString();
+	}
 }
